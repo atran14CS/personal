c34641924c51ddb5df238792c8328618e3c0b129
Akira Tran <118871037+atran14CS@users.noreply.github.com>
2024-05-10 19:27:10 -0700

    changed username

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -3,7 +3,7 @@
 
 const CONFIG = {
   github: {
-    username: 'atran14', // Your GitHub org/user name. (This is the only required config)
+    username: 'atran14CS', // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
